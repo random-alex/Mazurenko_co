de947a9c2cacf250e356adf02878f64aaa25665d
random-alex <aleksei.zotov.ml@gmail.com>
2018-04-23 13:50:41 +0300

    new data

--- stat.R
+++ stat.R
@@ -1,13 +1,13 @@
 
 # prereq ------------------------------------------------------------------
 
-dir <- 'data/j_05'
+dir <- 'data/j_1'
 
 
 # read file ---------------------------------------------------------------
 
 df <- tibble(dirs = list.files(dir,pattern = 'task[0-9]{1}\\.out\\.xml|[0-9]{2}\\.out\\.xml',full.names = T)) %>% 
-  mutate(data = map(dirs,my_read_xml_aveg)) %>% 
+  mutate(data = map(dirs,my_read_xml_aveg,param = c('J','L','T','LATTICE'))) %>% 
   unnest() %>% 
   select(-dirs) %>% 
   mutate_at(vars(value,J,L,temp,error),funs(as.numeric))
@@ -22,8 +22,11 @@ df_corr <- tibble(dirs = list.files(dir,pattern = 'task[0-9]{1}\\.out\\.xml|[0-9
 # average param plot ------------------------------------------------------
 
 df %>% 
-  ggplot(aes(temp,value)) +
+  ggplot(aes(temp,value,col = as.factor(LATTICE))) +
   geom_line() +
   geom_pointrange(aes(ymin = value - error,ymax = value + error)) +
   facet_wrap(c('parameter'),scales = 'free') + 
+  scale_color_discrete(name = 'Тип решетки', labels = c('Нестандартная','Стандартная')) +
+  xlab('Температура, T/J') + 
+  ylab('Значения измеряемого параметра') +
   theme_bw()
